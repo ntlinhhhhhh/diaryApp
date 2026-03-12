3f8fd33ab36904d69020c107de4c94f22e20ada0
hana2615 <160174186+ntlinhhhhhh@users.noreply.github.com>
2026-03-12 17:31:19 +0700

    add email setting

---
+++ DiaryApp.Application/Configurations/EmailSettings.cs
@@ -0,0 +1,9 @@
+namespace DiaryApp.Domain.Configurations;
+
+public class EmailSettings
+{
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string Host { get; set; } = "smtp.gmail.com";
+    public int Port { get; set; } = 587;
+}
