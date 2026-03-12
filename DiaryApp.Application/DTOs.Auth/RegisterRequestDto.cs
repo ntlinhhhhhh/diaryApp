8d2bc0f75ac8a55d62aed37a11245bbbfaf00081
hana2615 <160174186+ntlinhhhhhh@users.noreply.github.com>
2026-03-12 11:42:25 +0700

    change register request

--- DiaryApp.Application/DTOs.Auth/RegisterRequestDto.cs
+++ DiaryApp.Application/DTOs.Auth/RegisterRequestDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DiaryApp.Application.DTOs.Auth;
 
 public class RegisterRequestDto
 {
+    [Required(ErrorMessage = "Email là bắt buộc")]
     public required string Email { get; set; }
+    
+    [Required(ErrorMessage = "Password là bắt buộc")]
     public required string Password { get; set; }
     public required string Name { get; set; }
 }
