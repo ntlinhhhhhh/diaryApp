17f6684de760fde0908dbdc0fc5362cf6ef68797
hana2615 <160174186+ntlinhhhhhh@users.noreply.github.com>
2026-03-13 00:44:44 +0700

    change controller

--- DiaryApp.API/Controllers/UserController.cs
+++ DiaryApp.API/Controllers/UserController.cs
@@ -12,15 +12,14 @@ namespace DiaryApp.API.Controllers;
 public class UserController(IUserService userService) : ControllerBase
 {
     private readonly IUserService _userService = userService;
+    private string CurrentUserId => User.FindFirstValue(ClaimTypes.NameIdentifier)!;
 
     [HttpGet("me")]
     public async Task<IActionResult> GetMyProfile()
     {
         try
         {
-            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
-            
-            var profile = await _userService.GetProfileAsync(userId);
+            var profile = await _userService.GetProfileAsync(CurrentUserId);
             return Ok(profile);
         }
         catch (Exception ex)
@@ -34,10 +33,64 @@ public class UserController(IUserService userService) : ControllerBase
     {
         try
         {
-            string userId = User.FindFirstValue(ClaimTypes.NameIdentifier)!;
-            
-            await _userService.UpdateProfileAsync(userId, request);
+            await _userService.UpdateProfileAsync(CurrentUserId, request);
             return Ok(new { message = "Cập nhật hồ sơ thành công!" });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+
+    [HttpGet("Search")]
+    public async Task<IActionResult> SearchUsers([FromQuery] string name, [FromQuery] int limit)
+    {
+        try
+        {
+            var users = await _userService.SearchUsersAsync(name, limit);
+            return Ok(users);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+
+    [HttpGet("me/themes")]
+    public async Task<IActionResult> GetMyThemes()
+    {
+        try
+        {
+            var themes = await _userService.GetMyThemeIdsAsync(CurrentUserId);
+            return Ok(themes);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+
+    [HttpPost("me/theme/buy")]
+    public async Task<IActionResult> BuyTheme([FromBody] BuyThemeRequestDto request)
+    {
+        try
+        {
+            await _userService.BuyThemeAsync(CurrentUserId, request);
+            return Ok(new { message = "Mua giao diện thành công" });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+
+    [HttpPut("me/themes/active")]
+    public async Task<IActionResult> ChangeTheme([FromBody] UpdateThemeRequestDto request)
+    {
+        try
+        {
+            await _userService.ChangeActiveThemeAsync(CurrentUserId, request);
+            return Ok(new { message = "Thay đổi giao diện thành công!" });
         }
         catch (Exception ex)
         {
