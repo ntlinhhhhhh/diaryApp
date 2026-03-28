e1b336e3e6fc63225ba24cca1a3370bd0991c2e9
hana2615 <160174186+ntlinhhhhhh@users.noreply.github.com>
2026-03-28 19:14:31 +0700

    change firebase

--- backend/DiaryApp.Infrastructure/Data/FirestoreProvider.cs
+++ backend/DiaryApp.Infrastructure/Data/FirestoreProvider.cs
@@ -7,19 +7,9 @@ public class FirestoreProvider
 {
     public FirestoreDb Database { get; }
 
-        public FirestoreProvider(IConfiguration configuration)
-        {
-            // path from appsettings.json
-            string filepath = configuration["Firebase:ServiceAccountPath"] 
-                              ?? throw new ArgumentNullException("Firebase path is missing");
-            
-            // projectId from appsettings.json
-            string projectId = configuration["Firebase:ProjectId"] 
-                               ?? throw new ArgumentNullException("Project ID is missing");
-
-            // env valuable
-            Environment.SetEnvironmentVariable("GOOGLE_APPLICATION_CREDENTIALS", filepath);
-
-            Database = FirestoreDb.Create(projectId);
-        }
+        public FirestoreProvider(FirestoreDb firestoreDb)
+    {
+        // Gán nó vào biến của class
+        Database = firestoreDb;
+    }
 }
